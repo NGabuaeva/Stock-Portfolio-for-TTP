0d630490348edff21c1fdde5e97092c5320b5f2f
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-03-12 19:41:00 -0400

    change

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const pkg = require('../../package.json');
 const databaseName =
   pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 const db = new Sequelize(
-  `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
     dialect: 'postgres',
